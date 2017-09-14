d537abb3eb3eaf2bbc6e41b03461beaaf46c00e0
wawrzyniak julien <wawrzyniakjulien@gmail.com>
2017-09-14 19:54:41 -0400

    Add mdbootstrap framework

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,27 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
-                <router-link to="/artist">Artist</router-link>
-            </li>
-        </ul>
-    </nav>
+
+  <nav class="navbar navbar-expand-lg navbar-dark indigo">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/album">Album</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/artist">Artist</router-link>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+
 </template>
 
 <style>
