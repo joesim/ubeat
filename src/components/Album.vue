201fccbb4486ee9eb865fefb750b968092995dab
Victor Moin <victor.moin@hei.yncrea.fr>
2017-09-22 16:32:39 -0400

    albums style

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,8 +1,49 @@
 <template>
-    <div>
-        <h1>Album</h1>
-        <div>GLO-3102 Album page</div>
+    <div class="container content">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="z-depth-3">
+                    <img src="http://artisticmanifesto.com/wp-content/uploads/2014/06/sam-smith-in-the-lonely-hour-track-list-album-cover-1-e1402968875856.jpg" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-md-8">
+                <h1>In the lonely hour</h1>
+                <h2>Sam Smith</h2>
+                <h3>Pop-rock</h3>
+                <div class="row">
+                    <div class="col">
+                        <p>16/08/2013 <br> 3 tracks</p>
+                    </div>
+                    <div class="col">
+                        <a href="https://geo.itunes.apple.com/ca/album/evolve/id1233502263?mt=1&app=music" style="display:inline-block;overflow:hidden;background:url(//linkmaker.itunes.apple.com/assets/shared/badges/en-us/music-lrg.svg) no-repeat;width:110px;height:40px;background-size:contain;"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <table class="table table-hover liste z-depth-1 text-center">
+            <tbody>
+                <tr>
+                <th scope="row">#1</th>
+                <td>Stay With Me</td>
+                <td>5:33</td>
+                <td><button class="dark-red waves-effect waves-light"><i class="fa fa-caret-right mr-1"></i> Play</button></td>
+                </tr>
+                <tr>
+                <th scope="row">#2</th>
+                <td>Stay With Me</td>
+                <td>4:48</td>
+                <td><button class="dark-red waves-effect waves-light"><i class="fa fa-caret-right mr-1"></i> Play</button></td>
+                </tr>
+                <tr>
+                <th scope="row">#3</th>
+                <td>Stay With Me</td>
+                <td>3:59</td>
+                <td><button class="dark-red waves-effect waves-light"><i class="fa fa-caret-right mr-1"></i> Play</button></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+
 </template>
 
 <style>
