356e922818d61e1ee7f81c3edea691fcace48e3d
alexandre wali <adalex.aw@gmail.com>
2017-09-26 00:50:58 -0400

    Artist page version 1

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,8 +1,69 @@
 <template>
-    <div>
-        <h1>Artist</h1>
-        <div>GLO-3102 Artist page</div>
+  <div class="container content">
+    <div class="row mb-md-4">
+      <div class="col-md-4 hm-black-strong">
+        <a href="https://itunes.apple.com/ca/artist/michael-jackson/id32940" target="_blank">
+          <div class="view overlay hm-zoom z-depth-3 rounded">
+            <img src="http://www.lovemarks.com/wp-content/uploads/lovemark-michael-jackson-standard-600x600.jpg" class="img-fluid" alt="artist picture">
+            <div class="mask flex-center waves-effect waves-light">
+              <p class="white-text">See on iTunes</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-md-8">
+        <h1>Michael Jackson</h1>
+        <h2 class="light-blue-text"><em>Pop</em></h2>
+        <div class="row">
+          <div class="col-md-3">
+            <a href="https://itunes.apple.com/ca/artist/michael-jackson/id32940" target="_blank">
+              <div class="hoverable rounded-circle hm-zoom">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/ITunes_12.2_logo.png/240px-ITunes_12.2_logo.png" class="img-fluid" alt="iTunes logo">
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="card">
+      <div class="card-body">
+        <table class="table table-hover">
+          <thead>
+          <tr class="light-blue-text">
+            <th>#</th>
+            <th>Album Name</th>
+            <th>Date</th>
+            <th>Tracks</th>
+            <th>Total Length</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Xscape</td>
+            <td>2014</td>
+            <td>8</td>
+            <td>32:25</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Michael</td>
+            <td>2010</td>
+            <td>10</td>
+            <td>42:13</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Invincible</td>
+            <td>2001</td>
+            <td>16</td>
+            <td>76:08</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
